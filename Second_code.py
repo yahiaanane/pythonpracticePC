c6cc4b2c9ac49761ec24850046d8f8dfae2250f9
Yahia Anane <yahiaanane@gmail.com>
2021-05-19 18:17:22 +0200

    added a second comment

--- Second_code.py
+++ Second_code.py
@@ -2,3 +2,4 @@ print("first_line form PC")
 print("Second_line form PC")
 print("Second_line form HubGit")
 # I add a comment and I don't give a fuck
+# I add a a second comment and I don't give a fuck
